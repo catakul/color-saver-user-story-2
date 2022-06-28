a5f987d9b5b1f47ea1924f2ac028a83e1b53c5d9
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-06-28 09:59:05 +0200

    add color api

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,7 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./ColorBox.css";
 
+const apiEndpoint = "https://www.thecolorapi.com/id";
+
 export const ColorBox = ({ color, onDelete, onChange }) => {
+  const [colorName, setColorName] = useState("");
+  useEffect(() => {
+    const fetchColorName = async () => {
+      try {
+        const response = await fetch(`${apiEndpoint}?hex=${color.slice(1)}`);
+
+        if (response.ok) {
+          const data = await response.json();
+          setColorName(data.name.value);
+        } else {
+          throw new Error(`Server returned ${response.status}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (color) {
+      fetchColorName();
+    }
+  }, [color]);
+
   const handleClick = () => {
     navigator.clipboard
       .writeText(color)
@@ -20,6 +43,7 @@ export const ColorBox = ({ color, onDelete, onChange }) => {
       }}
       onClick={handleClick}
     >
+      <p>{colorName}</p>
       <input
         type="text"
         value={color}
